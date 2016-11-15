e635ee1919fc1568b890815acaee2249d2db4616
Carlos Rodríguez <carlos@beatbcn.com>
2016-11-15 18:00:30 +0100

    Initial Commit

---
+++ picture.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Executes the Realsense code that generates both images and saves them to the current directory and afterwards calls the python script that creates the POST request to the server.
+./libs/librealsense/photoboard/main && python picture.py
+
