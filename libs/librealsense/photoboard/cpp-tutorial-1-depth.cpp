ed48ab8c196c1ef9dcf105543e0c4fc9ef9d0c3a
Carlos Rodríguez <carlos@beatbcn.com>
2016-12-07 17:13:49 +0100

    Fix trial

--- libs/librealsense/photoboard/cpp-tutorial-1-depth.cpp
+++ libs/librealsense/photoboard/cpp-tutorial-1-depth.cpp
@@ -101,7 +101,7 @@ int main() try
         printf("\n\n%s", buffer3);
 
         if (changed){
-          exec("./main");
+          std::system("./main");
           printf("\nPhoto taken\n");
           changed = false;
         }
@@ -116,16 +116,4 @@ catch(const rs::error & e)
     printf("rs::error was thrown when calling %s(%s):\n", e.get_failed_function().c_str(), e.get_failed_args().c_str());
     printf("    %s\n", e.what());
     return EXIT_FAILURE;
-}
-
-std::string exec(const char* cmd) {
-    char buffer[128];
-    std::string result = "";
-    std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
-    if (!pipe) throw std::runtime_error("popen() failed!");
-    while (!feof(pipe.get())) {
-        if (fgets(buffer, 128, pipe.get()) != NULL)
-            result += buffer;
-    }
-    return result;
 }
