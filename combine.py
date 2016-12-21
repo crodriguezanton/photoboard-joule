94443a8fb6894dfca162df82b549a9df64026386
Carlos Rodríguez <carlos@beatbcn.com>
2016-12-21 12:11:20 +0100

    Fix

--- combine.py
+++ combine.py
@@ -16,7 +16,7 @@ for im in images:
         new_im.paste(im.crop((0, 0, w/2, h)), (0,0))
     else:
         new_im.paste(im.crop((w/2, 0, w, h)), (w/2,0))
-  left=False
+    left=False
 
 new_im.save('photoboard-image-COLOR.png')
 print "Saved"
