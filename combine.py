5e182cf10983cf4a274642936ecd29636ca181d3
Carlos Rodríguez <carlos@beatbcn.com>
2016-12-21 12:06:40 +0100

    Combine

--- combine.py
+++ combine.py
@@ -8,7 +8,7 @@ total_width = max(widths)
 max_height = max(heights)
 
 new_im = Image.new('RGB', (total_width, max_height))
-
+print "Combining Images"
 left = True
 for im in images:
     w, h = im.size
@@ -18,4 +18,5 @@ for im in images:
         new_im.paste(im.crop((w/2, 0, w, h)), (w/2,0))
   left=False
 
-new_im.save('final-image.png')
+new_im.save('photoboard-image-COLOR.png')
+print "Saved"
--- picture.sh
+++ picture.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Executes the Realsense code that generates both images and saves them to the current directory and afterwards calls the python script that creates the POST request to the server.
-./libs/librealsense/photoboard/main && python staticpic.py
+./libs/librealsense/photoboard/main2 && python combine.py && python staticpic.py
