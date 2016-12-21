331171042653a76c58f4c9bfdb6d04dc7eb62702
Carlos Rodríguez <carlos@beatbcn.com>
2016-12-21 11:07:23 +0100

    Demo Settings

--- picture.sh
+++ picture.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # Executes the Realsense code that generates both images and saves them to the current directory and afterwards calls the python script that creates the POST request to the server.
-./libs/librealsense/photoboard/main && python picture.py
-
+./libs/librealsense/photoboard/main && python staticpic.py
---
+++ staticpic.py
@@ -0,0 +1,3 @@
+from dummypic import upload
+
+upload()
